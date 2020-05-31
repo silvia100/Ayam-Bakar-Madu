ff8a928e2f14e6cbb2b0405a221cb3294d9f186d
Silvia Dhea Safira <silviadsaff@student.uns.ac.id>
2020-05-31 22:47:50 +0700

    operator_aritmatika

---
+++ operator_aritmatika.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 08:36:07 2020
+
+@author: hp
+"""
+
+UTS = 0
+UAS = 0
+NA = 0
+
+UTS = 95
+UAS = 85
+
+NA = (UTS+UAS)/2
+
+print(NA)
+
+tugas = 89
+setengah_tugas = tugas/2
+sepertugas = 0
+sepertugas = tugas/3
+print(setengah_tugas)
+print(sepertugas)
+
+print(11 % 2)
+print(10 % 2)
+
+print(3**290)
